bfc250795a7866cf5b3afa69f100da7a9c5e500b
paul.rutten <paul.rutten@monkeyproofsolutions.nl>
2025-04-22 09:08:37 +0200

    Revert "Split model modules in a main section and run function."
    This reverts commit 97aae4b67ad65ea0dd024555d8b3933bedc7a6d7.

--- src/generativezoo/CondFM.py
+++ src/generativezoo/CondFM.py
@@ -1,36 +1,33 @@
-    from models.FM.CondFlowMatching import CondFlowMatching
-    from data.Dataloaders import *
-    from utils.util import parse_args_CondFlowMatching
-    import wandb
-    
-def run(args):
-    
-    
-    if args.train:
-    train_loader, input_size, channels = pick_dataset(args.dataset, batch_size = args.batch_size, normalize=True, num_workers=args.num_workers, size=args.size)
-    model = CondFlowMatching(args, input_size, channels)
-    model.train_model(train_loader)
-    wandb.finish()
-    
-    elif args.sample:
-    _, input_size, channels = pick_dataset(args.dataset, batch_size = 1, normalize=True, size=args.size)
-    model = CondFlowMatching(args, input_size, channels)
-    model.load_checkpoint(args.checkpoint)
-    model.sample(args.num_samples, train=False)
-    elif args.fid:
-    _, input_size, channels = pick_dataset(args.dataset, batch_size = 1, normalize=True, size=args.size)
-    model = CondFlowMatching(args, input_size, channels)
-    model.load_checkpoint(args.checkpoint)
-    model.fid_sample()
-    
-    elif args.translation:
-    val_loader, input_size, channels = pick_dataset(args.dataset, batch_size = 16, normalize=True, size=args.size, mode='val')
-    model = CondFlowMatching(args, input_size, channels)
-    model.load_checkpoint(args.checkpoint)
-    model.image_translation(val_loader)
-    else:
-    raise ValueError("Invalid mode, please specify train or sample mode.")
+from models.FM.CondFlowMatching import CondFlowMatching
+from data.Dataloaders import *
+from utils.util import parse_args_CondFlowMatching
+import wandb
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+
     args = parse_args_CondFlowMatching()
-    run(args)
+
+    if args.train:
+        train_loader, input_size, channels = pick_dataset(args.dataset, batch_size = args.batch_size, normalize=True, num_workers=args.num_workers, size=args.size) 
+        model = CondFlowMatching(args, input_size, channels)
+        model.train_model(train_loader)
+        wandb.finish()
+
+    elif args.sample:
+        _, input_size, channels = pick_dataset(args.dataset, batch_size = 1, normalize=True, size=args.size)
+        model = CondFlowMatching(args, input_size, channels)
+        model.load_checkpoint(args.checkpoint)
+        model.sample(args.num_samples, train=False)
+    elif args.fid:
+        _, input_size, channels = pick_dataset(args.dataset, batch_size = 1, normalize=True, size=args.size)
+        model = CondFlowMatching(args, input_size, channels)
+        model.load_checkpoint(args.checkpoint)
+        model.fid_sample()
+
+    elif args.translation:
+        val_loader, input_size, channels = pick_dataset(args.dataset, batch_size = 16, normalize=True, size=args.size, mode='val')
+        model = CondFlowMatching(args, input_size, channels)
+        model.load_checkpoint(args.checkpoint)
+        model.image_translation(val_loader)
+    else:
+        raise ValueError("Invalid mode, please specify train or sample mode.")
--- src/generativezoo/RF.py
+++ src/generativezoo/RF.py
@@ -1,24 +1,21 @@
-    from data.Dataloaders import pick_dataset
-    from models.FM.RectifiedFlows import RF
-    from utils.util import parse_args_RectifiedFlows
-    import wandb
-    
-def run(args):
-    
-    
-    
-    if args.train:
-    train_loader, input_size, channels = pick_dataset(args.dataset, batch_size = args.batch_size, normalize=True, num_workers=args.num_workers, size=args.size)
-    model = RF(args, input_size, channels)
-    model.train_model(train_loader)
-    wandb.finish()
-    
-    elif args.sample:
-    _, input_size, channels = pick_dataset(args.dataset, batch_size = 1, normalize=True, size=args.size)
-    model = RF(args, input_size, channels)
-    model.load_checkpoint(args.checkpoint)
-    model.sample(16)
+from data.Dataloaders import pick_dataset
+from models.FM.RectifiedFlows import RF
+from utils.util import parse_args_RectifiedFlows
+import wandb
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+
     args = parse_args_RectifiedFlows()
-    run(args)
+
+
+    if args.train:
+        train_loader, input_size, channels = pick_dataset(args.dataset, batch_size = args.batch_size, normalize=True, num_workers=args.num_workers, size=args.size)
+        model = RF(args, input_size, channels)
+        model.train_model(train_loader)
+        wandb.finish()
+
+    elif args.sample:
+        _, input_size, channels = pick_dataset(args.dataset, batch_size = 1, normalize=True, size=args.size)
+        model = RF(args, input_size, channels)
+        model.load_checkpoint(args.checkpoint)
+        model.sample(16)
